8490a416761962add50170e2ff4f6240ffcba369
Srijan Sarkar <srijanb463@gmail.com>
2026-01-04 00:25:50 +0530

    fixed login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
-import { LoginForm } from "@/components/Login-form"
+import { LoginForm } from "../components/Login-form"
 
 export default function LoginPage() {
   return (
