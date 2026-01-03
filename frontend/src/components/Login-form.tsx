dcfbbe8c5d2251c3ae9436553a9dfb2eb32b5e5b
Srijan Sarkar <srijanb463@gmail.com>
2026-01-03 23:10:04 +0530

    loginform fixed

--- frontend/src/components/Login-form.tsx
+++ frontend/src/components/Login-form.tsx
@@ -1,21 +1,21 @@
 import { useState } from "react"
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "../lib/utils"
+import { Button } from "../components/ui/button"
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "../components/ui/card"
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
   FieldSeparator,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+} from "../components/ui/field"
+import { Input } from "../components/ui/input"
 import { useNavigate } from "react-router-dom"
 
 
